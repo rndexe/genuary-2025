1203410b9544e8db7e144a3b53382df0376118bd
rndexe <himanshu.ernde@gmail.com>
2025-01-06 14:46:43 +0530

    jan 2

--- src/dates/2.jsx
+++ src/dates/2.jsx
@@ -1,30 +1,71 @@
-import { useRef, useState } from 'react'
-import { Canvas, useFrame } from '@react-three/fiber'
-import { Environment } from '@react-three/drei'
+import { Instance, Instances, useTexture } from '@react-three/drei'
+import { useMemo, useRef } from 'react'
+import { MathUtils } from 'three'
+import { useFrame } from '@react-three/fiber'
 
-export default function Cone(props) {
-  // This reference gives us direct access to the THREE.Mesh object
-  const ref = useRef()
-  // Hold state for hovered and clicked events
-  const [hovered, hover] = useState(false)
-  const [clicked, click] = useState(false)
-  // Subscribe this component to the render-loop, rotate the mesh every frame
-  useFrame((state, delta) => (ref.current.rotation.z += delta))
-  // Return the view, these are regular Threejs elements expressed in JSX
+export default function Layers(props) {
   return (
     <>
-      <ambientLight intensity={Math.PI / 2} />
-      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
-      <mesh
-        {...props}
-        ref={ref}
-        scale={clicked ? 1.5 : 1}
-        onClick={(event) => click(!clicked)}
-        onPointerOver={(event) => (event.stopPropagation(), hover(true))}
-        onPointerOut={(event) => hover(false)}>
-        <coneGeometry />
-        <meshStandardMaterial color={hovered ? 'hotpink' : 'red'} />
-      </mesh>
+      <Mover />
+      <CameraRig />
     </>
   )
+}
+
+function Mover() {
+  const ref1 = useRef()
+  const ref2 = useRef()
+
+  const speed = 2,
+    s = 20
+  useFrame((state, delta) => {
+    ref1.current.position.z += speed * delta
+    ref2.current.position.z += speed * delta
+    if (ref1.current.position.z > s) ref1.current.position.z = ref2.current.position.z - s
+    if (ref2.current.position.z > s) ref2.current.position.z = ref1.current.position.z - s
+  })
+  return (
+    <>
+      <group ref={ref1}>
+        <Clouds count={1000} />
+      </group>
+      <group ref={ref2}>
+        <Clouds count={1000} />
+      </group>
+    </>
+  )
+}
+
+function Clouds({ count }) {
+  const texture = useTexture('./cloud10.png')
+
+  const cloudsData = useMemo(() => {
+    return Array.from({ length: count }, () => ({
+      position: [MathUtils.randFloatSpread(10), MathUtils.randFloat(-4, 2), MathUtils.randFloat(-10, 10)],
+      scale: Math.random() * Math.random() * 4,
+      rotation: Math.random() * Math.PI,
+    }))
+  }, [count])
+
+  useFrame((state, delta) => {})
+  return (
+    <Instances limit={count}>
+      <planeGeometry args={[1, 1]} />
+      <meshBasicMaterial map={texture} transparent depthWrite={false} depthTest={false} opacity={0.5} />
+      {cloudsData.map((data, i) => (
+        <Instance
+          key={i}
+          position={data.position}
+          scale={[data.scale, data.scale, 1]}
+          rotation={[0, 0, data.rotation]}
+        />
+      ))}
+    </Instances>
+  )
+}
+
+function CameraRig() {
+  useFrame((state) => {
+    state.camera.position.lerp({ x: state.pointer.x, y: 1.5 + state.pointer.y / 2, z: 8 }, 0.1)
+  })
 }
